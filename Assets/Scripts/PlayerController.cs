b9b719c24e13be99f26e63d148653aa5686df9c8
Maria Saveleva <mariasaveleva@protonmail.com>
2022-02-27 18:32:37 +0200

    Updates of layout and PlayerController script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        // Debug.Log(Input.GetAxis("Horizontal"));
+        // Debug.Log(Input.GetAxis("Vertical"));
+        // Debug.Log("======");
+        moveInput *= Time.deltaTime;
+
+        if (!moveInput.Equals(Vector2.zero))
+        {
+            transform.Translate(new Vector3(moveInput.x, moveInput.y, 0));    
+        }
         
+        // transform.position += new Vector3(moveVelocity.x, moveVelocity.y, 0);
     }
 }
